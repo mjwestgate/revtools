ca44a0733912a8829409a3bd2c3f99a5229ed0d7
mjwestgate <martinjwestgate@gmail.com>
2017-07-10 16:20:47 +0200

    make pretty.citations robust to references that contain limited data

--- R/pretty.citations.R
+++ R/pretty.citations.R
@@ -5,26 +5,35 @@ pretty.citations<-function(
 	details=TRUE # whether to allow or suppress bibliographic details - name, year, journal
 	){
 	if(details){
-		# author info
-		# remove any additional characters that display affiliations (i.e. those after last ".")
-		author.data<-unlist(lapply(strsplit(x$author, ""), function(a){
-			dot.lookup<-a %in% "."
-			if(any(dot.lookup)){a<-a[1:max(which(dot.lookup))]}
-			return(paste(a, collapse=""))
-			}))
-		author.data<-unlist(lapply(strsplit(author.data, ", "), function(a){paste(a[2], a[1], sep=" ")}))
-	
-		n.authors<-length(x$author)
-		if(n.authors>=4){n.authors<-4}
-		author.info<-switch(as.character(n.authors), 
-			"0"="Anon.",
-			"1"=author.data,
-			"2"={paste(author.data, collapse=" & ")},
-			"3"={paste(author.data[1], ", ", author.data[2], " & ", author.data[3], sep="")},
-			"4"={paste(author.data[1], ", ", author.data[2], " et al.", sep="")})
-	
+
+		if(any(names(x)=="author")){
+			# author info
+			# remove any additional characters that display affiliations (i.e. those after last ".")
+			author.data<-unlist(lapply(strsplit(x$author, ""), function(a){
+				dot.lookup<-a %in% "."
+				if(any(dot.lookup)){a<-a[1:max(which(dot.lookup))]}
+				return(paste(a, collapse=""))
+				}))
+			author.data<-unlist(lapply(strsplit(author.data, ", "), function(a){paste(a[2], a[1], sep=" ")}))
+		
+			n.authors<-length(x$author)
+			if(n.authors>=4){n.authors<-4}
+			author.info<-switch(as.character(n.authors), 
+				"0"="Anon.",
+				"1"=author.data,
+				"2"={paste(author.data, collapse=" & ")},
+				"3"={paste(author.data[1], ", ", author.data[2], " & ", author.data[3], sep="")},
+				"4"={paste(author.data[1], ", ", author.data[2], " et al.", sep="")})
+		} else{author.info<-""}		
+
+
 		# paste info in the correct order
-		result<-paste(author.info, " (", x$year, ") ", x$title, ". ", x$journal, " ", x$volume, ": ", x$pages, sep="")
+		lookup.headers<-c("year", "title", "journal", "volume", "pages")
+		lookup.result<-lookup.headers %in% names(x)
+		if(all(lookup.result)){
+			result<-paste(author.info, " (", x$year, ") ", 
+				x$title, ". ", x$journal, " ", x$volume, ": ", x$pages, sep="")
+		}else{result<-paste(x[lookup.headers[lookup.result]], collapse=" ")}
 	
 		# add abstract if required
 		if(abstract){
