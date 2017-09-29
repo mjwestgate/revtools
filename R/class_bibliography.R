c8128168a2d84962cf850a0bee0d4958b642bc2f
mjwestgate <martinjwestgate@gmail.com>
2017-09-29 15:29:32 +1000

    bug fix to allow functions to call color palettes
    update NAMESPACE to call viridisLite functions and pipes; update
    summary functions to use ‘object’ rather than ‘x’ as the primary
    argument; specify viridisLite as package containing color palettes in
    start_review_window.

--- R/class_bibliography.R
+++ R/class_bibliography.R
@@ -2,17 +2,17 @@
 summary.bibliography<-function(object, ...){
 
 	# are any abstracts completely missing? 
-	null.check<-unlist(lapply(x, function(a){is.null(a$abstract)}))
-	null.count<-length(x)-length(which(null.check))
-	null.percent<-round((100/length(x)) * null.count, 1)
+	null.check<-unlist(lapply(object, function(a){is.null(a$abstract)}))
+	null.count<-length(object)-length(which(null.check))
+	null.percent<-round((100/length(object)) * null.count, 1)
 
 	# how many sources?
-	n.sources<-unlist(lapply(x, function(a){a$journal}))
+	n.sources<-unlist(lapply(object, function(a){a$journal}))
 	if(is.null(n.sources)==FALSE){
 		source.freq<-sort(xtabs(~n.sources), decreasing=TRUE)[1:5]
 		# put text together
 		result<-paste(
-			paste("Object of class 'bibliography' containing", length(x), "entries.", sep=" "), "\n\t",
+			paste("Object of class 'bibliography' containing", length(object), "entries.", sep=" "), "\n\t",
 			paste("Number containing abstracts: ", null.count, " (", null.percent, "%)", sep="") , "\n",
 			paste("Number of sources:", length(unique(n.sources)), sep=" "), "\n",
 			"Most common sources:", "\n\t",
@@ -21,7 +21,7 @@ summary.bibliography<-function(object, ...){
 		collapse="\n")
 	}else{
 		result<-paste(
-			paste("Object of class 'bibliography' containing", length(x), "entries.", sep=" "), "\n\t",
+			paste("Object of class 'bibliography' containing", length(object), "entries.", sep=" "), "\n\t",
 			paste("Number containing abstracts: ", null.count, " (", null.percent, "%)", sep="") , "\n",
 		sep="",
 		collapse="\n")
--- R/class_review_info.R
+++ R/class_review_info.R
@@ -2,19 +2,18 @@
 summary.review_info<-function(object, ...){
 	# 1: article summary
 	cat("Number of entries:\n")
-		cat(paste("  total: ", nrow(x$x), "\n", sep=""))
-		cat(paste("  sorted: ", length(which(x$x$tested)), "\n", sep=""))
-		cat(paste("  relevant: ", length(which(x$x$tested & x$x$selected)), "\n", sep=""))
-			cat()
+		cat(paste("  total: ", nrow(object$x), "\n", sep=""))
+		cat(paste("  sorted: ", length(which(object$x$tested)), "\n", sep=""))
+		cat(paste("  relevant: ", length(which(object$x$tested & object$x$selected)), "\n\n", sep=""))
 	# 2: data
 	cat("Topic modelling:\n")
-		if(any(grepl("LDA", x$model@call))){
+		if(any(grepl("LDA", object$model@call))){
 			cat("  model type: LDA\n")
 		}else{
 			cat("  model type: CTM\n")}
-		cat(paste("  number of topics: ", x$model@k, "\n", sep=""))
-		cat(paste("  number of unique words: ", ncol(x$dtm), "\n", sep=""))
+		cat(paste("  number of topics: ", object$model@k, "\n", sep=""))
+		cat(paste("  number of unique words: ", ncol(object$dtm), "\n", sep=""))
 		cat(paste("  mean number of words per article: ",  
-			round(mean(apply(x$dtm, 1, sum)), 0), "\n", sep=""))	
+			round(mean(apply(object$dtm, 1, sum)), 0), "\n", sep=""))	
 }
 # add something about content: most common words, perhaps name of the original file?
