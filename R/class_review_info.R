0d13f46cbe112f64547adfddf5529efe05d720a7
mjwestgate <martinjwestgate@gmail.com>
2017-09-29 15:38:18 +1000

    update to summary.review_info
    attempt #1 to avoid install error

--- R/class_review_info.R
+++ R/class_review_info.R
@@ -2,18 +2,19 @@
 summary.review_info<-function(object, ...){
 	# 1: article summary
 	cat("Number of entries:\n")
-		cat(paste("  total: ", nrow(object$x), "\n", sep=""))
-		cat(paste("  sorted: ", length(which(object$x$tested)), "\n", sep=""))
-		cat(paste("  relevant: ", length(which(object$x$tested & object$x$selected)), "\n\n", sep=""))
+		cat(paste0("  total: ", nrow(object$x), "\n"))
+		cat(paste0("  sorted: ", length(which(object$x$tested)), "\n"))
+		cat(paste0("  relevant: ", length(which(object$x$tested & object$x$selected)), "\n"))
+		cat()
 	# 2: data
 	cat("Topic modelling:\n")
 		if(any(grepl("LDA", object$model@call))){
 			cat("  model type: LDA\n")
 		}else{
 			cat("  model type: CTM\n")}
-		cat(paste("  number of topics: ", object$model@k, "\n", sep=""))
-		cat(paste("  number of unique words: ", ncol(object$dtm), "\n", sep=""))
-		cat(paste("  mean number of words per article: ",  
-			round(mean(apply(object$dtm, 1, sum)), 0), "\n", sep=""))	
+		cat(paste0("  number of topics: ", object$model@k, "\n"))
+		cat(paste0("  number of unique words: ", ncol(object$dtm), "\n"))
+		cat(paste0("  mean number of words per article: ",  
+			round(mean(apply(object$dtm, 1, sum)), 0), "\n"))	
 }
 # add something about content: most common words, perhaps name of the original file?
