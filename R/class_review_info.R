553e5f2951e12a16dec1d3bb68e39da348ed98f6
mjwestgate <martinjwestgate@gmail.com>
2018-02-05 09:42:47 +1100

    update to help files and function visibility
    updates include: prevent export of format_citation for user calls
    (print is more useful); simplify index; test new code code
    revtools-package help file

--- R/class_review_info.R
+++ R/class_review_info.R
@@ -1,20 +1,16 @@
 # methods for class(review_info)
 summary.review_info<-function(object, ...){
-	# 1: article summary
-	cat("Number of entries:\n")
-		cat(paste0("  total: ", nrow(object$x), "\n"))
-		cat(paste0("  sorted: ", length(which(object$x$tested)), "\n"))
-		cat(paste0("  relevant: ", length(which(object$x$tested & object$x$selected)), "\n"))
-		cat()
-	# 2: data
-	cat("Topic modelling:\n")
-		if(any(grepl("LDA", object$model@call))){
-			cat("  model type: LDA\n")
-		}else{
-			cat("  model type: CTM\n")}
-		cat(paste0("  number of topics: ", object$model@k, "\n"))
-		cat(paste0("  number of unique words: ", ncol(object$dtm), "\n"))
-		cat(paste0("  mean number of words per article: ",  
-			round(mean(apply(object$dtm, 1, sum)), 0), "\n"))	
+cat(
+	paste0("review_info object containing 5 entries:\n  info = data on ", 
+		nrow(object$info),
+		" citations sent to start_review_window\n  dtm = document term matrix (", 
+		nrow(object$dtm), " rows, ", ncol(object$dtm), 
+		" cols)\n  model = topic model with ",
+		object$model@k, 
+		" topics\n  plotinfo = list containing data for plots (x=articles, y=words, topic=topics)\n",
+		"  infostore = list containing review decisions data\nProgress: ",
+		length(which(object$infostore$x$selected)), " selected from ",
+		length(which(object$infostore$x$tested)), " decisions"
+	)
+)
 }
-# add something about content: most common words, perhaps name of the original file?
