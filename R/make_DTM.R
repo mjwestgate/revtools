83318692cdcbe046e95b1167c8c7da31c58bf495
Martin Westgate <martinjwestgate@gmail.com>
2019-10-15 16:21:06 +1100

    update to make_DTM
    - now all words of 3 words or fewer removed (not 2 words or fewer as previously)
    - shortest version of all words with the same stem is now chosen for display, not the most common
    - extra if statements to ensure ngram detection and stem replacement only go ahead when needed
    - fix bug where returning as a matrix discarded stemming and ngram improvements

--- R/make_DTM.R
+++ R/make_DTM.R
@@ -54,28 +54,32 @@ make_dtm <- function(
     ngrams <- ngrams[ngrams$freq > 2, ]
     ngrams <- ngrams[ngrams$freq > stats::quantile(ngrams$freq, ngram_quantile), ]
 
-    # split into pairs
-    ngram_list <- strsplit(ngrams$ngrams, " ")
-    ngram_df <- as.data.frame(
-      do.call(rbind, ngram_list),
-      stringsAsFactors = FALSE
-    )
+    if(nrow(ngrams) > 0){
+      # split into pairs
+      ngram_list <- strsplit(ngrams$ngrams, " ")
+      ngram_df <- as.data.frame(
+        do.call(rbind, ngram_list),
+        stringsAsFactors = FALSE
+      )
 
-    # remove small & stop words
-    keep_rows <- apply(ngram_df[, 1:2], 1, function(a, sw){
-      all(nchar(a) > 4) & !any(a %in% sw)
-    }, sw = stop_words)
-    if(any(keep_rows)){
-      ngram_df <- ngram_df[keep_rows, ]
-    }
-    source_text <- apply(ngram_df, 1, function(a){paste(a, collapse = " ")})
-    replacement_text <- apply(ngram_df, 1, function(a){paste(a, collapse = "_")})
+      # remove small & stop words
+      keep_rows <- apply(ngram_df[, 1:2], 1, function(a, sw){
+        all(nchar(a) > 4) & !any(a %in% sw)
+      }, sw = stop_words)
+      if(any(keep_rows)){
+        ngram_df <- ngram_df[keep_rows, ]
 
-    # replace in a loop
-    for(i in seq_along(source_text)){
-      x <- gsub(source_text[i], replacement_text[i], x)
-    }
-  }
+        source_text <- apply(ngram_df, 1, function(a){paste(a, collapse = " ")})
+        replacement_text <- apply(ngram_df, 1, function(a){paste(a, collapse = "_")})
+
+        # replace in a loop
+        for(i in seq_along(source_text)){
+          x <- gsub(source_text[i], replacement_text[i], x)
+        }
+
+      } # end if any kept ngrams
+    } # end if >0 ngrams
+  } # end if ngram_check
 
   # continue tm
   x <- tm::removeWords(x, stop_words)
@@ -90,7 +94,7 @@ make_dtm <- function(
 	dtm <- tm::DocumentTermMatrix(
     x = tm::Corpus(tm::VectorSource(x)),
     control = list(
-      wordLengths = c(3, Inf), # default
+      wordLengths = c(4, Inf), # default
       bounds = list(global = bound_values)
     )
   )
@@ -105,69 +109,77 @@ make_dtm <- function(
     stemmed = SnowballC::wordStem(dtm$dimnames$Terms),
     stringsAsFactors = FALSE
   )
-  # convert dtm to a df
-  dtm_df <- data.frame(
-    i = dtm$i, # articles
-    j = dtm$j, # words
-    v = dtm$v  # counts
-  )
-  # get sum of observations of each term
-  lookup$n <- unlist(lapply(
-    split(dtm_df$v, dtm_df$j), sum))
 
-  # determine which source word should replace the stemmed word
-  text_split <- split(lookup[, c("initial_n", "n")], lookup$stemmed)
-  text_match <- data.frame(
-    initial_n = unlist(lapply(text_split, function(a){a$initial_n})),
-    final_n = unlist(lapply(text_split,
-      function(a){
-        if(nrow(a) > 1){
-          rep(a$initial_n[order(a$n, decreasing = TRUE)[1]], nrow(a))
-        }else{
-          a$initial_n
-        }
-      }))
+  # does this contain duplicates?
+  if(base::anyDuplicated(lookup$stemmed) > 0){
+
+    # convert dtm to a df
+    dtm_df <- data.frame(
+      i = dtm$i, # articles
+      j = dtm$j, # words
+      v = dtm$v  # counts
     )
-  lookup <- merge(lookup, text_match, by = "initial_n")
+    # more efficient to use the shortest word, not the most common term
+    lookup$n <- nchar(lookup$initial)
 
-  # create new dtm_df that ranks by stemmed words
-  dtm_df$j_new <- lookup$final_n[dtm_df$j]
-  dtm_list <- split(dtm_df[c("j_new", "v")], dtm_df$i)
-  dtm_df2 <- do.call(rbind, lapply(
-    seq_along(dtm_list),
-    function(a, data){
-      result <- unlist(lapply(split(data[[a]]$v, data[[a]]$j_new), sum))
-      return(data.frame(
-        i = a,
-        j = as.numeric(names(result)),
-        v = result
-      ))
-    }, data = dtm_list
-  ))
+    # determine which source word should replace the stemmed word
+    text_split <- split(lookup[, c("initial_n", "n")], lookup$stemmed)
+    text_match <- data.frame(
+      initial_n = unlist(lapply(text_split, function(a){a$initial_n})),
+      final_n = unlist(lapply(text_split,
+        function(a){
+          if(nrow(a) > 1){
+            rep(a$initial_n[order(a$n, decreasing = FALSE)[1]], nrow(a))
+          }else{
+            a$initial_n
+          }
+        }))
+      )
+    lookup$final_n <- text_match$final_n[order(text_match$initial_n)]
 
-  # the new index has a higher maximum than length
-  # ensure index is continuous from 1 to length
-  unique_j <- sort(unique(lookup$final_n))
-  lookup2 <- data.frame(
-    index = seq_len(max(lookup$final_n)),
-    end = NA
-  )
-  lookup2$end[unique_j] <- seq_along(unique_j)
 
-  # use to create simple_triplet_matrix (faster to run LDA than class:matrix)
-  dtm2 <- slam::simple_triplet_matrix(
-    i = dtm_df2$i,
-    j = lookup2$end[dtm_df2$j],
-    v = dtm_df2$v,
-    dimnames = list(
-      "Docs" = as.character(seq_along(unique(dtm_df2$i))),
-      "Terms" = lookup$initial[sort(unique(lookup$final_n))]
+    # create new dtm_df that ranks by stemmed words
+    dtm_df$j_new <- lookup$final_n[dtm_df$j]
+    dtm_list <- split(dtm_df[c("j_new", "v")], dtm_df$i)
+    dtm_df2 <- do.call(rbind, lapply(
+      seq_along(dtm_list),
+      function(a, data){
+        result <- unlist(lapply(split(data[[a]]$v, data[[a]]$j_new), sum))
+        return(data.frame(
+          i = a,
+          j = as.numeric(names(result)),
+          v = result
+        ))
+      }, data = dtm_list
+    ))
+
+    # the new index has a higher maximum than length
+    # ensure index is continuous from 1 to length
+    unique_j <- sort(unique(lookup$final_n))
+    lookup2 <- data.frame(
+      index = seq_len(max(lookup$final_n)),
+      end = NA
     )
-  )
+    lookup2$end[unique_j] <- seq_along(unique_j)
+
+    # use to create simple_triplet_matrix
+    dtm2 <- slam::simple_triplet_matrix(
+      i = dtm_df2$i,
+      j = lookup2$end[dtm_df2$j],
+      v = dtm_df2$v,
+      dimnames = list(
+        "Docs" = as.character(seq_along(unique(dtm_df2$i))),
+        "Terms" = lookup$initial[sort(unique(lookup$final_n))]
+      )
+    )
+
+  }else{
+    dtm2 <- dtm
+  } # end if duplicated
 
   # optionally return a traditional matrix
   if(return_matrix){
-    output <- as.matrix(dtm) # convert back to matrix
+    output <- as.matrix(dtm2) # convert back to matrix
     rownames(output) <- paste0(
       "V",
       seq_len(nrow(output))
